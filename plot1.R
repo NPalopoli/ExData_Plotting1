5a9a04ea49349221f3ab38710cafba57078d34db
Nicolas Palopoli <nicopalo@gmail.com>
2015-01-11 03:54:38 -0300

    Fixed comment in plot1.R

--- plot1.R
+++ plot1.R
@@ -21,6 +21,6 @@ dataSet <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
 as.numeric.factor <- function(x) {  # convert factor to numeric
   as.numeric(levels(x))[x]
 }
-png(filename="plot1.png", width=480, height=480)   # choose device.. 
+png(filename="plot1.png", width=480, height=480) 
 hist(as.numeric.factor(dataSet$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
